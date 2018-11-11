7e239cd7fa7e3ffc2a7af9b4d5401850180a0cc5
BeJust <pavlenok2000@gmail.com>
2018-11-11 04:39:59 +0300

    hope final version

---
+++ Drawer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace CG_LAB5
+{
+    public class Drawer
+    {
+        public static void DrawPicture(Graphics g, int x, int y)
+        {
+            Bitmap bitmap = new Bitmap(@"C:\2 курс\Компьютерная графика\CG-LAB5\CG-LAB5\120px-Sun.png");
+            g.DrawImage(bitmap, x, y);
+        }
+    }
+}
---
+++ MainWindow.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CG_LAB5
+{
+    public partial class MainWindow : Form
+    {   
+        int x = 325, y = 160, a = 10, b = 10;
+
+        private void MainWindow_Paint(object sender, PaintEventArgs e)
+        {
+            Draw(e.Graphics,e);
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Draw(Graphics g, PaintEventArgs e)
+        {
+            Bitmap bmp = new Bitmap(Width, Height);
+            Drawer.DrawPicture(e.Graphics, x, y);
+            e.Graphics.DrawImage(bmp, 0, 0);
+            bmp.Dispose();
+        }
+
+        private void MainWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            int dx = 10, dy = 10;
+
+
+            if (e.KeyValue == 37)
+            {
+                if (x > a - 10)
+                    x = x - dx;
+                Invalidate();
+            }
+
+            if (e.KeyValue == 39)
+            {
+                if (x < Width - 12.5 * a)
+                    x = x + dx;
+                Invalidate();
+            }
+            if (e.KeyValue == 38)
+            {
+                if (y > b - 10)
+                    y = y - dy;
+                Invalidate();
+            }
+            if (e.KeyValue == 40)
+            {
+                if (y < Height - 17.8 * b/2)
+                    y = y + dy;
+                Invalidate();
+            }
+        }
+    }
+    
+}
